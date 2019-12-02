f1327a079bd086c0cec3a4810db5c78838cb7e7a
AmaiaAretxalde <aaretxalde@gmail.com>
2019-12-02 16:37:50 +0100

    Login/registro

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
-const getDb = require('./getDb');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/teas');
+mongoose.connect('mongodb://localhost:27017/teashop');
+
+const Teas = mongoose.model('Teas', { id: String, name: String, basePrice: Number, funcionalidad: String, descripcion: String, stock: Number, sabor: String, aroma: String, image: String });
+const User = mongoose.model('User', { nombre: String, apellido: String, email:String, direccion: String, localidad: String, provincia: String, cp: String, telefono: String, password: String });
 
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
@@ -30,35 +32,28 @@ const LocalStrategy = require('passport-local').Strategy;
 
 passport.use(
     new LocalStrategy(
-        { usernameField: 'nombreUsuario' },
-        function (nombreUsuario, password, done) {
-            let db = getDb();
-            db.collection('usuarios').find({ nombreUsuario: nombreUsuario }).toArray(function (err, usuarios) {
+        { usernameField: 'email' },
+        function (email, password, done) {
+            User.find({ email: email }, function (err, usuarios){
                 if (usuarios.length === 0) {
                     return done(null, false);
                 }
                 const usuario = usuarios[0];
-                console.log(password)
-                console.log(usuario.password)
                 let result=(bcrypt.compareSync(password, usuario.password));
-                if(result) {
-                // if (password, usuario.password) {
-                    return done(null, usuario);
-                } else {
-                    return done(null, false);
-                }
-            });
-        }
-    )
-);
+                if (result) {
+                        return done(null, usuario);
+                    } else {
+                        return done(null, false);
+                };
+            })
+        }));
 
 passport.serializeUser(function (usuario, done) {
-    done(null, usuario.nombreUsuario);
+    done(null, usuario.email);
 });
 
 passport.deserializeUser(function (id, done) {
-    let db = getDb();
-    db.collection('usuarios').find({ nombreUsuario: id }).toArray(function (err, usuarios) {
+    User.find({ email: id }, function (err, usuarios) {
         if (usuarios.length === 0) {
             done(null, null);
         }
@@ -66,29 +61,31 @@ passport.deserializeUser(function (id, done) {
     });
 });
 
-
 //REGISTRO DE NUEVOS USUARIOS:
 app.post('/api/usuario/registro', function (req, res) {
 
     let cifrado = bcrypt.hashSync(req.body.password, 10);
     let nombre = req.body.nombre;
+    let apellido = req.body.apellido;
     let email = req.body.email;
-    let nombreUsuario = req.body.nombreUsuario;
-    // let password = req.body.password;
-    let db = getDb();
-    db.collection('usuarios').find({ nombreUsuario: nombreUsuario}).toArray(function (err, datos) {
+    let direccion = req.body.direccion;
+    let localidad = req.body.localidad;
+    let provincia = req.body.provincia;
+    let cp = req.body.cp;
+    let telefono = req.body.telefono;
+    let user = new User({ nombre: nombre, apellido: apellido, email: email, direccion: direccion, localidad: localidad, provincia: provincia, cp: cp, telefono: telefono, password: cifrado });
+    User.find({ email: email }, function (err, datos) {
         if (err !== null) {
-            res.send(err);
+            res.send({ mensaje: '404' });
             return;
         }
         if (datos.length !== 0) {
-            res.send({mensaje:'Error en el registro. Nombre de usuario ' + nombreUsuario + ' ya registrado. Elija otro, por favor.'});
+            res.send({ mensaje: 'Error en el registro. Email ' + email + ' ya registrado. Elija otro, por favor.' });
+        } else {
+            user.save();
+            res.send({ mensaje: 'Usuario registrado correctamente' });
         }
-        else {
-            db.collection('usuarios').insertOne({nombre:nombre, email:email, nombreUsuario:nombreUsuario, password:cifrado });
-            res.send({mensaje:'Usuario registrado correctamente'});
-        }
-    })
+    });
 });
 
 //COMPROBAR DATOS DE ACCESO PARA USUARIOS YA REGISTRADOS:
@@ -97,24 +94,24 @@ app.post('/api/usuario/login', passport.authenticate('local', {
     failureRedirect: '/api/usuario/fail'
 }));
 
-app.get('/api/usuario/fail', function(req, res) {
-    res.send({mensaje: 'denegado'})
+app.get('/api/usuario/fail', function (req, res) {
+    res.send({ mensaje: 'denegado' })
 });
 
 app.get('/api/usuario', function (req, res) {
     if (req.isAuthenticated() === false) {
-        return res.send({mensaje: 'denegado'});
+        return res.send({ mensaje: 'denegado' });
     }
-    res.send({mensaje: 'acceso correcto'});
+    res.send({ mensaje: 'acceso correcto' });
 });
 
 
 //LOGOUT
-app.get("/api/logout", function(req, res) {
+app.get('/api/logout', function (req, res) {
     req.logout();
-    res.send({mensaje: 'desconectado'});
-  });
+    res.send({ mensaje: 'desconectado' });
+});
 
-app.listen(3001, function(){
+app.listen(3001, function () {
     console.log('servidor en marcha');
 });
