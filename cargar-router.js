501ffb8da857e97cd88a4919dea2786d4468cf07
AmaiaAretxalde <aaretxalde@gmail.com>
2019-12-04 11:56:44 +0100

    busqueda por tipo

--- cargar-router.js
+++ cargar-router.js
@@ -6,7 +6,7 @@ router.post('/novedades', function (req, res) {
     let palabra1 = new RegExp(req.body.palabra1);
     let palabra2 = new RegExp(req.body.palabra2);
 
-    Tea.find({$or:[{descripcion: /navidad/}, {name:/Christmas/}]}, function (err, datos) {
+    Tea.find({$or:[{descripcion: /palabra1/}, {name:/palabra2/}]}, function (err, datos) {
         if (err !== null) {
             res.send({ mensaje: '404' });
             return;
@@ -15,5 +15,22 @@ router.post('/novedades', function (req, res) {
         }
     })
 });
+
+// OBTENER DATOS POR TIPO DE TÉ
+router.get('/:color', function (req, res) {
+    let color = new RegExp(req.params.color);
+    console.log(color)
+    
+    Tea.find({categoria:/color/}, function (err, datos) {
+        if (err !== null) {
+            res.send({ mensaje: '404' });
+            return;
+        } else {
+            res.send(datos)
+        }
+    })
+});
+
+
 
 module.exports = router;
