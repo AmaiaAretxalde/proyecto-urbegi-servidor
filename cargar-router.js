f5d78b2fb703f99eba430c686cdc57482ff40671
AmaiaAretxalde <aaretxalde@gmail.com>
2019-12-04 12:17:22 +0100

    cargar por tipo

--- cargar-router.js
+++ cargar-router.js
@@ -6,7 +6,7 @@ router.post('/novedades', function (req, res) {
     let palabra1 = new RegExp(req.body.palabra1);
     let palabra2 = new RegExp(req.body.palabra2);
 
-    Tea.find({$or:[{descripcion: /palabra1/}, {name:/palabra2/}]}, function (err, datos) {
+    Tea.find({$or:[{descripcion:palabra1}, {name:palabra2}]}, function (err, datos) {
         if (err !== null) {
             res.send({ mensaje: '404' });
             return;
@@ -21,7 +21,7 @@ router.get('/:color', function (req, res) {
     let color = new RegExp(req.params.color);
     console.log(color)
     
-    Tea.find({categoria:/color/}, function (err, datos) {
+    Tea.find({$or:[{descripcion:color}, {name:color}]}, function (err, datos) {
         if (err !== null) {
             res.send({ mensaje: '404' });
             return;
