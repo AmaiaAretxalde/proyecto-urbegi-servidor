fa699136afecbb2ae00aa28eb04b31cbbb883f2d
AmaiaAretxalde <aaretxalde@gmail.com>
2019-12-12 09:22:23 +0100

    dia 12

--- cesta-router.js
+++ cesta-router.js
@@ -19,53 +19,107 @@ router.post('/', async function (req, res) {
             console.log(id)
             return element.producto.id === id;
         });
-        if(posicion===undefined || posicion===(-1)){
-            cestaExistente.push({producto, unidades});
+        if (posicion === undefined || posicion === (-1)) {
+            cestaExistente.push({ producto, unidades });
             user.markModified('cesta');
             user.save();
             res.send({ mensaje: 'añadido a la cesta', logged: true, cestaExistente });
-        }else{
+        } else {
             cestaExistente[posicion].unidades += unidades;
             user.markModified('cesta');
             user.save();
             res.send({ mensaje: 'añadido a la cesta', logged: true, cestaExistente });
         };
-        }
     }
+}
 );
 
 
-//ELIMINAR DE CESTA
+//ELIMINAR PRODUCTO DE CESTA
 router.delete('/:id', async function (req, res) {
-    let id = req.params.id;
-    const user = req.user;
     if (req.isAuthenticated() === false) {
         return res.send({ mensaje: 'No estás logueado', logged: false });
     } else {
-        let userDocument = await User.findById(user._id);
-        userDocument.cesta = userDocument.cesta.filter(function (element) {
-            return element._id !== id;
+        let id = req.params.id;
+        const user = req.user;
+        let cestaExistente = user.cesta;
+        let posicion = cestaExistente.findIndex(function (element) {
+            console.log(element.producto.id)
+            console.log(id)
+            return element.producto.id === id;
         });
-        userDocument.save();
-        res.send({ mensaje: 'eliminado', cesta });
+        if (posicion === undefined || posicion === (-1)) {
+            res.send({ mensaje: 'este producto no existe en la cesta', logged: true, cestaExistente });
+        } else {
+            cestaExistente.splice(posicion, 1);
+            user.markModified('cesta');
+            await user.save();
+            res.send({ mensaje: 'producto eliminado de la cesta', logged: true, cestaExistente });
+        };
     }
 });
 
-// VER CESTA 
-router.get('/', function (req, res) {
-    let email = req.body.email
-    const user = req.user
+//MODIFICAR UNIDADES DE CESTA
+router.put('/', async function (req, res) {
+    let posicion;
+   
     if (req.isAuthenticated() === false) {
         return res.send({ mensaje: 'No estás logueado', logged: false });
     } else {
-        User.find({ email: user.email }, function (err, usuario) {
-            if (err !== null) {
-                console.log(err);
-                return;
-            }
-            res.send(usuario[0].cesta);
-        })
+        let id = req.body.id;
+        let unidades = req.body.unidades;
+        const user = req.user;
+        let cestaExistente = user.cesta;
+        posicion = cestaExistente.findIndex(function (element) {
+            console.log(element.producto.id)
+            console.log(id)
+            return element.producto.id === id;
+            
+        });
+   
+        if (posicion === undefined || posicion === (-1)) {
+            res.send({ mensaje: 'este producto no existe en la cesta', logged: true, cestaExistente });
+        } else {
+            cestaExistente[posicion].unidades += unidades;
+            if (cestaExistente[posicion].unidades === 0) {
+                cestaExistente.splice(posicion, 1);
+            };
+            user.markModified('cesta');
+            await user.save();
+            res.send({ mensaje: 'producto modificado de la cesta', logged: true, cestaExistente });
+        }
+    }
+});
+
+
+// VER CESTA 
+router.get('/', async function (req, res) {
+
+    const user = req.user
+    if (req.isAuthenticated() === false) {
+        res.send({ mensaje: 'No estás logueado', logged: false });
+    } else {
+        await res.send(user.cesta);
     };
+});
+
+
+//PASAR DE CESTA A PEDIDO
+router.get('/pedido', async function (req, res) {
+
+    const user = req.user
+    if (req.isAuthenticated() === false) {
+        res.send({ mensaje: 'No estás logueado', logged: false });
+    } else {
+        user.pedidos.push(user.cesta);
+        user.markModified('pedidos');
+        await user.save();
+        user.cesta = [];
+        user.markModified('cesta');
+        await user.save();
+
+        res.send({ mensaje: 'pedido realizado', logged: true, respuesta: pedidos });
+    }
 });
 
 
