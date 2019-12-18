422dccda15ec03b287a4be303fd1a78d6fbe3eee
Alicia Aguirre <alicia.ag.br@gmail.com>
2019-12-18 16:13:13 +0100

    te para copiar

---
+++ TeCopiar.js
@@ -0,0 +1,36 @@
+let teas = [{
+    "id": "1",
+    "mainImage": "https://dhb3yazwboecu.cloudfront.net/1007/fotosProducto/tes/10153_BerryFields_1000x1000_l.jpg",
+    "categoria": "Té negro",
+    "descripcion": "Té negro,frutas del bosque,remolacha y manzana",
+    "unidadesVendidas": 0,
+    "name": "Té negro berry fields",
+    "longDescription": [
+        "Té Negro combinado con una caprichosa y envolvente mezcla de frutos silvestres, manzana y remolacha que predominan sobre el característico sabor astringente del té negro.Un té que nos transporta a la naturaleza y nos recuerda a una dulce confitura casera.Té energizante y estimulante con todo el sabor y aroma de las frutas del bosque.Se puede disfrutar tanto en caliente como en frío.Completo en vitaminas y antioxidantes ¡nos llenará de vitalidad y energía!",
+        "",
+        "Funcionalidad: Energizante",
+        "",
+        "¡Quiero saber más!: No contiene alérgenos.",
+        "",
+        "Ingredientes: Té negro, manzana, escaramujo, remolacha,fresas del bosque, fresa, arándano, frambuesa, baya de sauco, grosella roja, aromas de fresa, grosella negra y mora.",
+        "",
+        "",
+        ""
+    ],
+    "caracteristicas": {
+        "aroma": {
+            "image": "https://dhb3yazwboecu.cloudfront.net/1007/icons/aroma.svg",
+            "texto": "Dulce con un ligero toque acidulado."
+        },
+        "sabor": {
+            "image": "https://dhb3yazwboecu.cloudfront.net/1007/icons/sabor.svg",
+            "texto": "Dulce afrutado"
+        },
+        "color": {
+            "image": "https://dhb3yazwboecu.cloudfront.net/1007/icons/color.svg",
+            "texto": "Castaño rojizo"
+        }
+    },
+    "stock": 261,
+    "basePrice": 7.95
+},]
